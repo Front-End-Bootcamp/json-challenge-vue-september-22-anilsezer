3cc8df946865d691ee1245b79514ee832cd415f6
Anıl Sezer <70068018+anilsezer@users.noreply.github.com>
2022-09-17 22:06:55 +0300

    Add files via upload

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,13 @@
-console.log('👻');
+import data from "../Data/data.json" assert {type: "json"};
+
+  const groups = data.reduce((groups, item) => {
+    const group = (groups[item.group] || []);
+    group.push(item);
+    groups[item.group] = group;
+    return groups;
+  }, {});
+  
+  console.log(groups);
+
+//   Source
+//   https://stackoverflow.com/questions/21776389/javascript-object-grouping 
